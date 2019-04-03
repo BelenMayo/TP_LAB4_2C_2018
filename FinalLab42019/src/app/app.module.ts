c77dc15cb8e8856e2c96c44582ce0f8ba151272f
BelenMayo <belenmaydi@hotmail.com>
2019-04-02 22:57:36 -0300

    Componentes

--- FinalLab42019/src/app/app.module.ts
+++ FinalLab42019/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PedidosComponent } from './components/pedidos/pedidos.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { EmpleadosComponent } from './components/empleados/empleados.component';
+import { MesasComponent } from './components/mesas/mesas.component';
+import { ReservasComponent } from './components/reservas/reservas.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PedidosComponent,
+    ClientesComponent,
+    EmpleadosComponent,
+    MesasComponent,
+    ReservasComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
