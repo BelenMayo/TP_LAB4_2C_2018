6c8d0e8ca72c259f856eddc258b1e6bab5f52f97
BelenMayo <belenmaydi@hotmail.com>
2019-04-20 23:51:29 -0300

    Formulario

--- FinalLab42019/src/app/app.routes.ts
+++ FinalLab42019/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ClientesComponent } from './components/clientes/clientes.component';
+import { EmpleadosComponent } from './components/empleados/empleados.component';
 import { HomeComponent } from './components/home/home.component';
 
 const APP_ROUTES: Routes= [
     {path: 'home', component: HomeComponent},
     {path: 'cliente', component: ClientesComponent},
+    {path: 'empleado', component: EmpleadosComponent},
     {path: '**', pathMatch: 'full' , redirectTo:'home'}
 ];
 
