5f5aa014343dadb6d03a12f26d11093d63dd4d87
BelenMayo <belenmaydi@hotmail.com>
2020-07-29 23:54:28 -0300

    Favicon

--- FinalLab42019/src/globales/variables_globales.ts
+++ FinalLab42019/src/globales/variables_globales.ts
@@ -1,2 +1,3 @@
-export const API_REF = "http://localhost"
+//export const API_REF = "http://localhost"
 //export const API_REF = "http://finallab4bm.rivmay.net"
+export const API_REF = "http://lacomandabm.rivmay.net"
